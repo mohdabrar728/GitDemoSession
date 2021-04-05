993a7a790807be879fa1a73ea1885a1eef098468
Kalyan-Nathari <64293871+Kalyan-Nathari@users.noreply.github.com>
2021-04-05 10:31:17 +0530

    Deleted third number

--- Exercise-2-Add numbers/index.js
+++ Exercise-2-Add numbers/index.js
@@ -1,9 +1,9 @@
 function addNumbers() {
   var num1 = document.getElementById("num1");
   var num2 = document.getElementById("num2");
-  var num3 = document.getElementById("num3");
+ 
 
-  var sum = Number(num1.value) + Number(num2.value) + Number(num3.value);
+  var sum = Number(num1.value) + Number(num2.value) ;
 
   var result = document.getElementById("result");
   result.innerHTML = sum;
