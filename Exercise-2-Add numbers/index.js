cf457a3fd60ff185e986f7a166b09868b50e1ff6
Kalyan-Nathari <64293871+Kalyan-Nathari@users.noreply.github.com>
2021-04-05 10:37:00 +0530

    Added 4 numbers to JS file

--- Exercise-2-Add numbers/index.js
+++ Exercise-2-Add numbers/index.js
@@ -1,9 +1,10 @@
 function addNumbers() {
   var num1 = document.getElementById("num1");
   var num2 = document.getElementById("num2");
- 
+  var num3 = document.getElementById("num3");
+  var num4 = document.getElementById("num4");
 
-  var sum = Number(num1.value) + Number(num2.value) ;
+  var sum = Number(num1.value) + Number(num2.value) + Number(num3.value) + Number(num4.value) ;
 
   var result = document.getElementById("result");
   result.innerHTML = sum;
